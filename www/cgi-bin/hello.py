c486a658d2459a5c15dbed23f95edf519c555d48
mbany <mbany@student.42.pl>
2025-10-31 21:17:14 +0100

    cgi python online show works

--- www/cgi-bin/hello.py
+++ www/cgi-bin/hello.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
-print("Content-Type: text/plain")
-print()
-print("Hello from Python CGI (hello.py)")
+# print("Content-Type: text/plain")
+# print()
+# print("Hello from Python CGI (hello.py)")
+for i in range (0,10,1):
+	print(f"{i} Hello from Python CGI (hello.py)")
